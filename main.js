5d6f1f69d366f16a85a64672a7c188932eb03211
Nishant Pai <nishantpai@Nishants-MacBook-Pro.local>
2023-08-07 17:22:15 +0530

    add basic text generation

--- main.js
+++ main.js
@@ -1,24 +1,12 @@
 import './style.css'
-import javascriptLogo from './javascript.svg'
-import viteLogo from '/vite.svg'
-import { setupCounter } from './counter.js'
-
-document.querySelector('#app').innerHTML = `
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="${viteLogo}" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://developer.mozilla.org/en-US/docs/Web/JavaScript" target="_blank">
-      <img src="${javascriptLogo}" class="logo vanilla" alt="JavaScript logo" />
-    </a>
-    <h1>Hello Vite!</h1>
-    <div class="card">
-      <button id="counter" type="button"></button>
-    </div>
-    <p class="read-the-docs">
-      Click on the Vite logo to learn more
-    </p>
-  </div>
-`
-
-setupCounter(document.querySelector('#counter'))
+import { write } from './svg.js'
+document.querySelector('#message').addEventListener('input', (e) => {
+  // add svg to preview
+  let svg = write(e.target.value, '#fff', 1, 5, 5);
+  document.querySelector('#preview').innerHTML = `
+    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 100 187" width="500">
+      <rect xmlns="http://www.w3.org/2000/svg" x="0" y="0" width="100%" height="100%" fill="#000"/>
+      ${svg}
+    </svg>
+    `
+});

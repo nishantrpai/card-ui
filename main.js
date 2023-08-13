b92987de0ef9f47dc0b26c01ab2bac8c125d79c4
Nishant Pai <nishantpai@Nishants-MacBook-Pro.local>
2023-08-13 19:09:59 +0530

    clear signature

--- main.js
+++ main.js
@@ -1,14 +1,18 @@
 import './style.css'
 import { write } from './svg.js'
 
-document.querySelector('#post-textinput')?.addEventListener('input', (e) => {
-  document.querySelector('#post-textoutput').innerHTML = write(e.target.value, '#fff', 3);
-});
-
-
 const canvas = document.querySelector("canvas");
 
 const signaturePad = new SignaturePad(canvas);
 
 // change pen color to white
 signaturePad.penColor = "rgb(255, 255, 255)";
+
+document.querySelector('#post-textinput')?.addEventListener('input', (e) => {
+  document.querySelector('#post-textoutput').innerHTML = write(e.target.value, '#fff', 3);
+});
+
+
+document.querySelector('#signature-clear')?.addEventListener('click', (e) => {
+  signaturePad.clear();
+});

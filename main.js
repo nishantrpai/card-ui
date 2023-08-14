40e4bedc17496d66ec2baf87777f2f14480ec0e5
Nishant Pai <nishantpai@Nishants-MacBook-Pro.local>
2023-08-14 17:07:03 +0530

    get params on submit

--- main.js
+++ main.js
@@ -1,6 +1,8 @@
 import './style.css'
 import { write } from './svg.js'
-
+let signature = '';
+let address = '';
+let message = '';
 const canvas = document.querySelector("canvas");
 
 const signaturePad = new SignaturePad(canvas);
@@ -9,9 +11,13 @@ const signaturePad = new SignaturePad(canvas);
 signaturePad.penColor = "rgb(255, 255, 255)";
 
 document.querySelector('#post-textinput')?.addEventListener('input', (e) => {
+  message = e.target.value;
   document.querySelector('#post-textoutput').innerHTML = write(e.target.value, '#fff', 3);
 });
 
+document.querySelector('#post-wallet-address')?.addEventListener('input', (e) => {
+  address = e.target.value;
+});
 
 document.querySelector('#signature-clear')?.addEventListener('click', (e) => {
   signaturePad.clear();
@@ -20,5 +26,10 @@ document.querySelector('#signature-clear')?.addEventListener('click', (e) => {
 document.querySelector('#signature-save')?.addEventListener('click', (e) => {
   // resize to 100x100
   let data = signaturePad.toDataURL('image/svg+xml');
+  signature = data;
   document.querySelector('#signature-output').src = data;
+});
+
+document.querySelector('#send-postcard')?.addEventListener('click', (e) => {
+  console.log(signature, address, message);
 });
